e97f0efee476ca4a4a6b7afba8917a24ab7d5446
Lukas Schmid <dimschlukas@gmail.com>
2023-07-23 20:56:56 +0200

    Initial commit

---
+++ config.h
@@ -0,0 +1,5 @@
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR // Sets the default mode, if none has been set
+#define RGB_MATRIX_DEFAULT_HUE 127 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5 // Sets the layer count for VIA_ENABLE
